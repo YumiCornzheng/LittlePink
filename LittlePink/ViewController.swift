8dfa264f12df3987a6f8284a151ab3bbbdfc4a6c
yuming.zheng <yuming.zheng@aitmed.com>
2023-04-27 17:11:33 +0800

    Initial Commit

---
+++ LittlePink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink
+//
+//  Created by yuming.zheng on 2023/4/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
